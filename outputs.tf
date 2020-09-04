c6578dfc298340044434dab025b2582c6f2e0c95
searcebhavin <69896928+searcebhavin@users.noreply.github.com>
2020-09-04 12:26:42 +0530

    Update outputs.tf
    Updated outputs file to have account_id as an additional output.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "email" {
   description = "The fully qualified email address of the created service account."
   value       = google_service_account.service_account.email
 }
+
+output "account_id" {
+  description = "The account ID of the created service account."
+  value       = google_service_account.service_account.account_id
+}
